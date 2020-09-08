0cdefa38dad70ef63b0ee1ed3f2ef9b4c797944e
Margui <margui.berjon@gmail.com>
2020-09-08 12:02:58 +0200

    create date funtion for footer

--- server/index.js
+++ server/index.js
@@ -15,6 +15,16 @@ app.set('views', path.join(__dirname, './views'));
 //load the public static folder
 app.use(express.static('public'));
 
+//show current year
+app.use((req, res, next) => {
+
+  //Create new date
+  const date = new Date();
+  res.locals.currentDate = date.getFullYear();
+  return next();
+})
+
+
 //load routes
 app.use('/', routes());
 
